74ad2335eda0079f11da3f5b9df319c08a067d05
senwa105 <senwa105@gmail.com>
2023-01-24 12:04:01 -0500

    added test for row and col count getters

--- src/matrix.test.cpp
+++ src/matrix.test.cpp
@@ -1,1 +1,16 @@
 #include <gtest/gtest.h>
+#include "matrix.hpp"
+
+TEST(MatrixTest, GetRowCount) {
+    Matrix4f a;
+    EXPECT_EQ(a.GetRowCount(), 4);
+    Matrix<int, 19, 23> b;
+    EXPECT_EQ(b.GetRowCount(), 19);
+}
+
+TEST(MatrixTest, GetColCount) {
+    Matrix4f a;
+    EXPECT_EQ(a.GetColCount(), 4);
+    Matrix<int, 19, 23> b;
+    EXPECT_EQ(b.GetColCount(), 23);
+}
