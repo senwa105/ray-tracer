1c95513a3ac949b50f8e3d8e746a724311ff3532
senwa105 <senwa105@gmail.com>
2023-01-24 11:22:10 -0500

    implemented operators () and <<

--- src/matrix.hpp
+++ src/matrix.hpp
@@ -7,7 +7,7 @@
 template <typename T, size_t M, size_t N>
 class Matrix {
 private:
-    std::array<T, M*N> entries_;
+    std::array<T, M*N> entries_{};
 
 public:
     // construct zero matrix by default
@@ -24,33 +24,59 @@ public:
     T& operator()(size_t row, size_t col);
     const T& operator()(size_t row, size_t col) const;
 
-    // scalar addition
-    friend Matrix<T, M, N> operator+(const Matrix<T, M, N>& m, const T& s);
-    friend Matrix<T, M, N> operator+(const T& s, const Matrix<T, M, N>& m);
-    // scalar subtraction
-    friend Matrix<T, M, N> operator-(const Matrix<T, M, N>& m, const T& s);
-    friend Matrix<T, M, N> operator-(const T& s, const Matrix<T, M, N>& m);
-    // scalar multiplication
-    friend Matrix<T, M, N> operator*(const Matrix<T, M, N>& m, const T& s);
-    friend Matrix<T, M, N> operator*(const T& s, const Matrix<T, M, N>& m);
+    // // scalar addition
+    // friend Matrix<T, M, N> operator+<>(const Matrix<T, M, N>& m, const T& s);
+    // friend Matrix<T, M, N> operator+<>(const T& s, const Matrix<T, M, N>& m);
+    // // scalar subtraction
+    // friend Matrix<T, M, N> operator-<>(const Matrix<T, M, N>& m, const T& s);
+    // friend Matrix<T, M, N> operator-<>(const T& s, const Matrix<T, M, N>& m);
+    // // scalar multiplication
+    // friend Matrix<T, M, N> operator*<>(const Matrix<T, M, N>& m, const T& s);
+    // friend Matrix<T, M, N> operator*<>(const T& s, const Matrix<T, M, N>& m);
 
-    // matrix addition
-    friend Matrix<T, M, N> operator+(const Matrix<T, M, N>& m1, const Matrix<T, M, N>& m2);
-    // matrix subtraction
-    friend Matrix<T, M, N> operator-(const Matrix<T, M, N>& m1, const Matrix<T, M, N>& m2);
-    // matrix multiplication
-    template <int O>
-    friend Matrix<T, M, O> operator*(const Matrix<T, M, N>& m1, const Matrix<T, N, O>& m2);
+    // // matrix addition
+    // friend Matrix<T, M, N> operator+<>(const Matrix<T, M, N>& m1, const Matrix<T, M, N>& m2);
+    // // matrix subtraction
+    // friend Matrix<T, M, N> operator-<>(const Matrix<T, M, N>& m1, const Matrix<T, M, N>& m2);
+    // // matrix multiplication
+    // template <int O>
+    // friend Matrix<T, M, O> operator*(const Matrix<T, M, N>& m1, const Matrix<T, N, O>& m2);
 
-    // negation
-    Matrix<T, M, N> operator-() const;
+    // // negation
+    // Matrix<T, M, N> operator-() const;
 
-    // comparison
-    friend bool operator==(const Matrix& m1, const Matrix& m2);
-    friend bool operator!=(const Matrix& m1, const Matrix& m2);
+    // // comparison
+    // friend bool operator==<>(const Matrix& m1, const Matrix& m2);
+    // friend bool operator!=<>(const Matrix& m1, const Matrix& m2);
 
     // operator <<
-    friend std::ostream& operator<<(std::ostream& os, const Matrix<T,m,n>& m);
+    friend std::ostream& operator<<<>(std::ostream& os, const Matrix<T, M, N>& m);
 };
+
+template <typename T, size_t M, size_t N>
+Matrix<T, M, N> operator+(const Matrix<T, M, N>& m, const T& s) {
+    
+}
+
+template <typename T, size_t M, size_t N>
+T& Matrix<T, M, N>::operator()(size_t row, size_t col) {
+    return entries_[row*M + col];
+}
+
+template <typename T, size_t M, size_t N>
+const T& Matrix<T, M, N>::operator()(size_t row, size_t col) const {
+    return entries_[row*M + col];
+}
+
+template <typename T, size_t M, size_t N>
+std::ostream& operator<<(std::ostream& os, const Matrix<T, M, N>& m) {
+    for (int i = 0; i < M; i++) {
+        for (int j = 0; j < N; j++)
+            os << m(i, j) << ' ';
+        os << '\n';
+    }
+
+    return os;
+}
 
 #endif
