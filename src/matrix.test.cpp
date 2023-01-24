acf80faee301afec940fd15387f589ad979baf43
senwa105 <senwa105@gmail.com>
2023-01-24 18:35:49 -0500

    added comparison tests for floats/doubles

--- src/matrix.test.cpp
+++ src/matrix.test.cpp
@@ -14,3 +14,21 @@ TEST(MatrixTest, GetColCount) {
     Matrix<int, 19, 23> b;
     EXPECT_EQ(b.GetColCount(), 23);
 }
+
+TEST(MatrixTest, FloatComparison) {
+    Matrix2f a({1.0f, 2.0f, 3.0f, 4.0f});
+    Matrix2f b({1.1f, 2.2f, 3.3f, 4.4f});
+    Matrix2f c({1.000001f, 2.000002f, 3.000003f, 4.000004f});
+
+    EXPECT_FALSE(a == b);
+    EXPECT_TRUE(a == c);
+}
+
+TEST(MatrixTest, DoubleComparison) {
+    Matrix2f a({1.0f, 2.0f, 3.0f, 4.0f});
+    Matrix2f b({1.1f, 2.2f, 3.3f, 4.4f});
+    Matrix2f c({1.000001f, 2.000002f, 3.000003f, 4.000004f});
+
+    EXPECT_FALSE(a == b);
+    EXPECT_TRUE(a == c);
+}
