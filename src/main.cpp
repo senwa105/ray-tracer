a166cde199cbad52b1cafbe14c0afda1f36b5b32
senwa105 <senwa105@gmail.com>
2023-02-01 22:56:12 -0500

    still BROKEN

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,11 @@
 #include "matrix.hpp"
 
 int main() {
-    
+    // Matrix2i a({1, 2, 3, 4});
+    // Matrix2i b({3, 4, 5, 6});
+    // std::cout << ((a + 2) == b) << '\n';
+
+    // Matrix2f c;
+    // Matrix2f d({1.5f, 1.5f, 1.5f, 1.5f});
+    // EXPECT_EQ(c + 1.5, d);
 }
