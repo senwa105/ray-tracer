9ac2facd9469ccacc6a0de71cecd07ff98b31b94
senwa105 <senwa105@gmail.com>
2023-08-21 19:43:54 -0400

    add Inverse test

--- src/matrix.test.cpp
+++ src/matrix.test.cpp
@@ -285,3 +285,16 @@ TEST(MatrixTest, IsInvertible) {
     EXPECT_TRUE(a.IsInvertible());
     EXPECT_FALSE(b.IsInvertible());
 }
+
+TEST(MatrixTest, Inverse) {
+    Matrix4f a{{-5,  2,  6, -8,
+                 1, -5,  1,  8,
+                 7,  7, -6, -7,
+                 1, -3,  7,  4}};
+    Matrix4f b{{ 0.21805,  0.45113,  0.24060, -0.04511,
+                -0.80827, -1.45677, -0.44361,  0.52068,
+                -0.07895, -0.22368, -0.05263,  0.19737,
+                -0.52256, -0.81391, -0.30075,  0.30639}};
+    EXPECT_EQ(a.Inverse(), b);
+    EXPECT_EQ(a.Inverse() * a, Matrix4f::Identity());
+}
