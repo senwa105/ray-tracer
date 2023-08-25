9a941ec901166355f2395f3ea9b73841344a5a5b
senwa105 <senwa105@gmail.com>
2023-08-25 01:58:30 -0400

    add Comparison test

--- src/color.test.cpp
+++ src/color.test.cpp
@@ -19,3 +19,19 @@ TEST_F(ColorTest, Getters) {
     EXPECT_FLOAT_EQ(a.green(), 0.22);
     EXPECT_FLOAT_EQ(a.blue(), 0.33);
 }
+
+TEST_F(ColorTest, Comparison) {
+    Color aa({0.11, 0.22, 0.33});
+    Color bb({0.5, 0.6, 0.7});
+    
+    EXPECT_EQ(aa, a);
+    EXPECT_EQ(bb, b);
+
+    EXPECT_NE(a, b);
+    EXPECT_NE(aa, bb);
+}
+
+// TEST_F(ColorTest, Addition) {
+//     Color c({0.61, 0.82, 1.03});
+//     EXPECT_EQ(a + b, c);
+// }
