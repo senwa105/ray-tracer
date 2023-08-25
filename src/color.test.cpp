ebc1cce58f2fad4dd09457424665fa3579cd6ce6
senwa105 <senwa105@gmail.com>
2023-08-25 01:53:04 -0400

    use fixture

--- src/color.test.cpp
+++ src/color.test.cpp
@@ -3,8 +3,18 @@
 
 using namespace RT;
 
-TEST(ColorTest, Getters) {
-    Color a({0.11, 0.22, 0.33});
+class ColorTest : public ::testing::Test {
+protected:
+    void SetUp() override {
+        a = Color({0.11, 0.22, 0.33});
+        b = Color({0.5, 0.6, 0.7});
+    }
+
+    Color a;
+    Color b;
+};
+
+TEST_F(ColorTest, Getters) {
     EXPECT_FLOAT_EQ(a.red(), 0.11);
     EXPECT_FLOAT_EQ(a.green(), 0.22);
     EXPECT_FLOAT_EQ(a.blue(), 0.33);
