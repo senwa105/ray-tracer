7a69668091308c96e6ff4c0e5db888408de333d2
senwa105 <senwa105@gmail.com>
2023-08-26 23:22:46 -0400

    fully implement CanvasToPPM

--- src/canvas.hpp
+++ src/canvas.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <string>
 #include <format>
+#include <cmath>
 #include "color.hpp"
 
 namespace RT {
@@ -43,8 +44,47 @@ public:
     }
 };
 
+unsigned int Clamp(const float unclamped, const unsigned int max) {
+    if (unclamped < 0)
+        return 0;
+    if (unclamped > 1)
+        return max;
+    return std::round(unclamped * max);
+}
+
 std::string CanvasToPPM(const Canvas& canvas) {
-    return std::format("P3\n{} {}\n255\n", canvas.GetWidth(), canvas.GetHeight());
+    const size_t MAX_VALUE = 255;
+    const size_t MAX_LINE_LENGTH = 70;
+
+    // Header
+    std::string ppm = std::format("P3\n{} {}\n255\n", canvas.GetWidth(), canvas.GetHeight());
+    
+    // Payload
+    for (size_t y = 0; y < canvas.GetHeight(); ++y) {
+        std::string line{};
+
+        for (size_t x = 0; x < canvas.GetWidth(); ++x) {
+            Color pixel = canvas.PixelAt(x, y);
+            std::string pixelString = std::format("{} {} {} ",
+                                                   Clamp(pixel.red(), MAX_VALUE),
+                                                   Clamp(pixel.green(), MAX_VALUE),
+                                                   Clamp(pixel.blue(), MAX_VALUE));
+
+            if (line.length() + pixelString.length() > MAX_LINE_LENGTH) {
+                line[line.length() - 1] = '\n';
+                ppm += line;
+                line.clear();
+            }
+
+
+            line += pixelString;
+        }
+
+        line[line.length() - 1] = '\n';
+        ppm += line;
+    }
+
+    return ppm;
 }
 
 }
