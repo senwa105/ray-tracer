d404bd321b5c5dbe6c230b5c9999f73edfdb5d92
senwa105 <senwa105@gmail.com>
2023-08-27 04:13:20 -0400

    rename Translation to Translate

--- src/transformations.hpp
+++ src/transformations.hpp
@@ -13,7 +13,7 @@ Matrix::Vector4f Vector(const float x, const float y, const float z) {
     return Matrix::Vector4f({x, y, z, 0});
 }
 
-Matrix::Matrix4f Translation(const float x, const float y, const float z) {
+Matrix::Matrix4f Translate(const float x, const float y, const float z) {
     Matrix::Matrix4f t = Matrix::Matrix4f::Identity();
     t(0, 3) = x;
     t(1, 3) = y;
--- tests/transformations.test.cpp
+++ tests/transformations.test.cpp
@@ -4,7 +4,7 @@
 TEST(TransformationTest, Translation) {
     auto p = RT::Point(-3, 4, 5);
     auto v = RT::Vector(-3, 4, 5);
-    auto t = RT::Translation(5, -3, 2);
+    auto t = RT::Translate(5, -3, 2);
 
     auto a = RT::Point(2, 1, 7);
     auto b = RT::Point(-8, 7, 3);
