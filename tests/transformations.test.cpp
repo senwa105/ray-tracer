cead85a1fb51b3122cca6860df6d5e8cc7a5a6dd
senwa105 <senwa105@gmail.com>
2023-08-27 04:24:10 -0400

    add Scale and Reflect tests

--- tests/transformations.test.cpp
+++ tests/transformations.test.cpp
@@ -1,7 +1,7 @@
 #include <gtest/gtest.h>
 #include "transformations.hpp"
 
-TEST(TransformationTest, Translation) {
+TEST(TransformationTest, Translate) {
     auto p = RT::Point(-3, 4, 5);
     auto v = RT::Vector(-3, 4, 5);
     auto t = RT::Translate(5, -3, 2);
@@ -12,4 +12,26 @@ TEST(TransformationTest, Translation) {
     EXPECT_EQ(t * p, a);
     EXPECT_EQ(t.Inverse() * p, b);
     EXPECT_EQ(t * v, v);
+}
+
+TEST(TransformationTest, Scale) {
+    auto p = RT::Point(-4, 6, 8);
+    auto v = RT::Vector(-4, 6, 8);
+    auto t = RT::Scale(2, 3, 4);
+
+    auto a = RT::Point(-8, 18, 32);
+    auto b = RT::Vector(-8, 18, 32);
+    auto c = RT::Vector(-2, 2, 2);
+
+    EXPECT_EQ(t * p, a);
+    EXPECT_EQ(t * v, b);
+    EXPECT_EQ(t.Inverse() * v, c);
+}
+
+TEST(TransformationTest, Reflect) {
+    auto p = RT::Point(2, 3, 4);
+    auto t = RT::Scale(-1, 1, 1);
+    auto a = RT::Point(-2, 3, 4);
+
+    EXPECT_EQ(t * p, a);
 }
