0434a65172e1e8fdc2ae5cdf3c3493533dc186b2
senwa105 <senwa105@gmail.com>
2023-08-27 02:45:14 -0400

    refactor to allow clamped color class

--- src/color.hpp
+++ src/color.hpp
@@ -5,31 +5,83 @@
 
 namespace RT {
 
-class Color {
-private:
-    Matrix::Vector3f rgb_{};
+template<typename T>
+class ColorBase {
+protected:
+    Matrix::Matrix<T, 3, 1> rgb_{};
 
-public:
-    Color() noexcept = default;
+    ColorBase() noexcept = default;
 
-    Color(const std::array<float, 3> list) noexcept 
+    ColorBase(const std::array<T, 3> list) noexcept 
         : rgb_(list)
     {}
 
-    Color(const Matrix::Vector3f mat) noexcept
+    ColorBase(const Matrix::Matrix<T, 3, 1> mat) noexcept
         : rgb_(mat)
     {}
 
+public:
     const float red() const { return rgb_.X(); }
     const float green() const { return rgb_.Y(); }
     const float blue() const { return rgb_.Z(); }
 
-    friend bool operator==(const Color& a, const Color& b) {
+    friend bool operator==(const ColorBase& a, const ColorBase& b) {
         return a.rgb_ == b.rgb_;
     }
 
-    friend bool operator!=(const Color& a, const Color& b) {
+    friend bool operator!=(const ColorBase& a, const ColorBase& b) {
         return a.rgb_ != b.rgb_;
+    }
+
+
+};
+
+namespace {
+
+template<typename T>
+T Clamp(const float unclamped, const T max) {
+    if (unclamped < 0)
+        return 0;
+    if (unclamped > 1)
+        return max;
+    return std::round(unclamped * max);
+}
+
+}
+
+class ColorClamped8Bit : public ColorBase<unsigned int> {
+public:
+    ColorClamped8Bit() noexcept = default;
+
+    ColorClamped8Bit(const std::array<unsigned int, 3> list) noexcept 
+        : ColorBase(list)
+    {}
+
+    ColorClamped8Bit(const Matrix::Matrix<unsigned int, 3, 1> mat) noexcept
+        : ColorBase(mat)
+    {} 
+};
+
+class Color : public ColorBase<float> {
+public:
+    Color() noexcept = default;
+
+    Color(const std::array<float, 3> list) noexcept 
+        : ColorBase(list)
+    {}
+
+    Color(const Matrix::Vector3f mat) noexcept
+        : ColorBase(mat)
+    {}
+
+    ColorClamped8Bit Clamp8Bit() const {
+        const unsigned int MAX_VALUE = 255;
+
+        return ColorClamped8Bit({
+            Clamp<unsigned int>(this->red(), MAX_VALUE),
+            Clamp<unsigned int>(this->green(), MAX_VALUE),
+            Clamp<unsigned int>(this->blue(), MAX_VALUE),
+        });
     }
 
     friend Color operator+(const Color& a, const Color& b) {
