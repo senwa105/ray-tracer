6d8d381fa2ee3453c0dbf0e08706deb8ff83c4eb
senwa105 <senwa105@gmail.com>
2023-08-28 00:48:14 -0400

    add SphereIntersection tests

--- tests/ray.test.cpp
+++ tests/ray.test.cpp
@@ -2,6 +2,8 @@
 #include "color.hpp"
 
 #include "ray.hpp"
+#include "shapes.hpp"
+#include "matrix.hpp"
 
 using namespace RT;
 
@@ -21,4 +23,52 @@ TEST(RayTest, Position) {
     EXPECT_EQ(Position(r, 1), Point(3, 3, 4));
     EXPECT_EQ(Position(r, -1), Point(1, 3, 4));
     EXPECT_EQ(Position(r, 2.5), Point(4.5, 3, 4));
+}
+
+TEST(RayTest, SphereIntersection) {
+    auto r = Ray(Point(0, 0, -5), Vector(0, 0, 1));
+    auto s = Shapes::Sphere();
+
+    auto xs = Intersect(s, r);
+    EXPECT_EQ(xs.size(), 2);
+    EXPECT_EQ(xs[0], 4.0);
+    EXPECT_EQ(xs[1], 6.0);
+}
+
+TEST(RayTest, SphereTangentIntersection) {
+    auto r = Ray(Point(0, 1, -5), Vector(0, 0, 1));
+    auto s = Shapes::Sphere();
+
+    auto xs = Intersect(s, r);
+    EXPECT_EQ(xs.size(), 2);
+    EXPECT_TRUE(Matrix::ApproxEqual(xs[0], 5.0));
+    EXPECT_TRUE(Matrix::ApproxEqual(xs[1], 5.0));
+}
+
+TEST(RayTest, SphereNoIntersection) {
+    auto r = Ray(Point(0, 2, -5), Vector(0, 0, 1));
+    auto s = Shapes::Sphere();
+
+    auto xs = Intersect(s, r);
+    EXPECT_EQ(xs.size(), 0);
+}
+
+TEST(RayTest, SphereInsideIntersection) {
+    auto r = Ray(Point(0, 0, 0), Vector(0, 0, 1));
+    auto s = Shapes::Sphere();
+
+    auto xs = Intersect(s, r);
+    EXPECT_EQ(xs.size(), 2);
+    EXPECT_TRUE(Matrix::ApproxEqual(xs[0], -1.0));
+    EXPECT_TRUE(Matrix::ApproxEqual(xs[1], 1.0));
+}
+
+TEST(RayTest, SphereRearIntersection) {
+    auto r = Ray(Point(0, 0, 5), Vector(0, 0, 1));
+    auto s = Shapes::Sphere();
+
+    auto xs = Intersect(s, r);
+    EXPECT_EQ(xs.size(), 2);
+    EXPECT_TRUE(Matrix::ApproxEqual(xs[0], -6.0));
+    EXPECT_TRUE(Matrix::ApproxEqual(xs[1], -4.0));
 }
