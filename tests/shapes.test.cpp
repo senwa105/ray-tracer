b16d96b7ef28cf2fb867d21476c894570da1f9ae
senwa105 <senwa105@gmail.com>
2023-08-28 05:24:44 -0400

    add Initialization and SetTransform tests

---
+++ tests/shapes.test.cpp
@@ -0,0 +1,15 @@
+#include <gtest/gtest.h>
+#include "shapes.hpp"
+
+TEST(SphereTest, Initialization) {
+    auto s = RT::Shapes::Sphere();
+    auto id = Matrix::Matrix4f::Identity();
+    EXPECT_EQ(s.GetTransform(), id);
+}
+
+TEST(SphereTest, SetTransform) {
+    auto s = RT::Shapes::Sphere();
+    auto t = Matrix::Matrix4f::Identity() * 4;
+    s.SetTransform(t);
+    EXPECT_EQ(s.GetTransform(), t);
+}
