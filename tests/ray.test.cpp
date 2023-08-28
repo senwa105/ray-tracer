754379be348feae082eb2fc88c3cad8167fef0d2
senwa105 <senwa105@gmail.com>
2023-08-27 23:48:10 -0400

    add Position test

--- tests/ray.test.cpp
+++ tests/ray.test.cpp
@@ -3,11 +3,22 @@
 
 #include "ray.hpp"
 
+using namespace RT;
+
 TEST(RayTest, Initialization) {
-    auto o = RT::Point(1, 2, 3);
-    auto d = RT::Vector(4, 5, 6);
-    auto r = RT::Ray(o, d);
+    auto o = Point(1, 2, 3);
+    auto d = Vector(4, 5, 6);
+    auto r = Ray(o, d);
 
     EXPECT_EQ(r.origin, o);
     EXPECT_EQ(r.direction, d);
+}
+
+TEST(RayTest, Position) {
+    auto r = Ray(Point(2, 3, 4), Vector(1, 0, 0));
+
+    EXPECT_EQ(Position(r, 0), Point(2, 3, 4));
+    EXPECT_EQ(Position(r, 1), Point(3, 3, 4));
+    EXPECT_EQ(Position(r, -1), Point(1, 3, 4));
+    EXPECT_EQ(Position(r, 2.5), Point(4.5, 3, 4));
 }
