88f99bf731aaac789f6ee30247528a3bc65caa7f
senwa105 <senwa105@gmail.com>
2023-08-29 16:52:09 -0400

    add NormalAt test

--- tests/shapes.test.cpp
+++ tests/shapes.test.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "shapes.h"
+#include "transformations.h"
 
 TEST(SphereTest, Initialization) {
     auto s = RT::Shapes::Sphere();
@@ -12,4 +13,29 @@ TEST(SphereTest, SetTransform) {
     auto t = Matrix::Matrix4f::Identity() * 4;
     s.SetTransform(t);
     EXPECT_EQ(s.GetTransform(), t);
+}
+
+TEST(SphereTest, NormalAt) {
+    auto s = RT::Shapes::Sphere();
+    auto n = RT::Vector(1, 0, 0);
+    auto p = RT::Point(1, 0, 0);
+    EXPECT_EQ(s.NormalAt(p), n);
+
+    n = RT::Vector(0, 1, 0);
+    p = RT::Point(0, 1, 0);
+    EXPECT_EQ(s.NormalAt(p), n);
+
+    n = RT::Vector(0, 0, 1);
+    p = RT::Point(0, 0, 1);
+    EXPECT_EQ(s.NormalAt(p), n);
+
+    n = RT::Vector(0, 1, 0);
+    p = RT::Point(0, 1, 0);
+    EXPECT_EQ(s.NormalAt(p), n);
+
+    float x = sqrt(3) / 3;
+    n = RT::Vector(x, x, x);
+    p = RT::Point(x, x, x);
+    EXPECT_EQ(s.NormalAt(p), n);
+    EXPECT_EQ(Normalize(n), n);
 }
