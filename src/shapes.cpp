f4522b0ffde0fb873120de890b66955e7d1de86b
senwa105 <senwa105@gmail.com>
2023-08-29 17:15:57 -0400

    update NormalAt for transformed Spheres

--- src/shapes.cpp
+++ src/shapes.cpp
@@ -5,7 +5,11 @@
 namespace RT::Shapes {
 
 Matrix::Vector4f Sphere::NormalAt(const Matrix::Vector4f point) const {
-    return Normalize(point - Point(0, 0, 0));
+    auto objectPoint = transform_.Inverse() * point;
+    auto objectNormal = objectPoint - Point(0, 0, 0);
+    auto normal = transform_.Inverse().Transpose() * objectNormal;
+    normal.W() = 0;
+    return Normalize(normal);
 }
 
 }
