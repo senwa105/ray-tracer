3eae710b1bfd8c4237bdb0d4e38578c40cc424d7
senwa105 <senwa105@gmail.com>
2023-08-31 11:58:50 -0400

    render lighting effects

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "matrix.hpp"
 #include "transformations.h"
 #include "ray.h"
+#include "light.h"
 #include "canvas.h"
 
 int main() {
@@ -10,7 +11,7 @@ int main() {
     const float WALL_Z = 10;
     const float WALL_SIZE = 7;
 
-    const int RESOLUTION = 100; 
+    const int RESOLUTION = 500; 
 
     auto camera = RT::Point(0, 0, CAMERA_Z);
 
@@ -18,22 +19,35 @@ int main() {
     float half = WALL_SIZE / 2;
 
     RT::Canvas canvas{RESOLUTION, RESOLUTION};
-    RT::Color color{{1, 0, 0}}; // Red
+
     RT::Shapes::Sphere shape{};
-    shape.SetTransform(RT::Shear(1, 0, 0, 0, 0, 0)
-                       * RT::Scale(0.5, 1, 1));
+    // shape.SetTransform(RT::Shear(1, 0, 0, 0, 0, 0)
+    //                    * RT::Scale(0.5, 1, 1));
+    auto mat = RT::Material();
+    mat.color = RT::Color({1, 0.2, 1});
+    shape.SetMaterial(mat);
+
+    auto lightPosition = RT::Point(-10, 10, -10);
+    auto lightColor = RT::Color({1, 1, 1});
+    auto light = RT::PointLight(lightPosition, lightColor);
 
     for (size_t y = 0; y < RESOLUTION; ++y) {
         float yWorld = half - pixelSize * y;
         for (size_t x = 0; x < RESOLUTION; ++x) {
             float xWorld = -half + pixelSize * x;
             auto position = RT::Point(xWorld, yWorld, WALL_Z);
-            RT::Ray r{camera, Normalize(position - camera)};
-            auto xs = RT::Intersect(shape, r);
+            auto ray = RT::Ray(camera, Normalize(position - camera));
+            auto xs = RT::Intersect(shape, ray);
 
             auto hit = RT::Hit(xs);
-            if (hit)
+            if (hit) {
+                auto point = RT::Position(ray, hit->t);
+                auto normal = hit->object->NormalAt(point);
+                auto eye = -ray.direction;
+
+                auto color = RT::Lighting(hit->object->GetMaterial(), light, point, eye, normal);
                 canvas.WritePixel(x, y, color);
+            }
         }
     }
 
