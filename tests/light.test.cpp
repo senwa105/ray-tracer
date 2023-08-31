77f027db3eee4a8e540c40d08b00dde5ec0ff6f9
senwa105 <senwa105@gmail.com>
2023-08-31 10:14:20 -0400

    add Material test

--- tests/light.test.cpp
+++ tests/light.test.cpp
@@ -9,3 +9,12 @@ TEST(LightTest, Initialization) {
     EXPECT_EQ(l.intensity, intensity);
     EXPECT_EQ(l.position, position);
 }
+
+TEST(LightTest, Material) {
+    auto m = RT::Material(RT::Color({1, 1, 1}), 0.1, 0.9, 0.9, 200);
+    EXPECT_EQ(m.color, RT::Color({1, 1, 1}));
+    EXPECT_FLOAT_EQ(m.ambient, 0.1);
+    EXPECT_FLOAT_EQ(m.diffuse, 0.9);
+    EXPECT_FLOAT_EQ(m.specular, 0.9);
+    EXPECT_FLOAT_EQ(m.shininess, 200);
+}
